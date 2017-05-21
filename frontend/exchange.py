364655c15592058cafcbbc09417d1bc32ba2ddc9
wqt-zst <981494130@qq.com>
2017-05-20 20:14:49 -0700

    the exchange.py is only used for ir_exchange.

--- frontend/exchange.py
+++ frontend/exchange.py
@@ -7,185 +7,166 @@ from frontend.ast import *
 from frontend.explain import *
 
 
+#program definition
+def programExc(programdata,name,filename,line_number,varlist,funclist,labellist,body)
+	programdata['object'] = "program_definition"
+	programdata['name'] = name
+	programdata['fiename'] = filename
+    programdata['line_number'] = line_number
+	programdata['variablelist'] = varlist
+	programdata['functionlist'] = funclist
+	programdata['labellist'] = labellist
+	programdata['body'] = body
+
+
 #function definition
-def functionExc(funcdata,funcNode)
+def functionExc(funcdata,name,filename,line_number,functype,paralist,varlist,labellist,body)
 	funcdata['object'] = "function_definition"
-	funcdata['filename'] = funcNode.position().path
-    funcdata['line_number'] = funcNode.position().lineno
-	funcdata['name'] = funcNode.header
-	funcdata['type'] = funcNode.type()
-	paralist=[]
-	varlistExplain(paralist,funcNode.attr)
+	funcdata['name'] = name
+	funcdata['filename'] = filename
+    funcdata['line_number'] = line_number
+	funcdata['type'] = functype
 	funcdata['parameterlsit'] = paralist
-	labellist=[]
-	body=[]
-	bodyExplain(labellist,body,funcNode.block)
+	funcdata['variablelsit'] = varlist
 	funcdata['labellist'] = labellist
 	funcdata['body'] = body
 
-```
-{
-    "object": "function_definition",
-    "filename": "test.cl",
-    "line_number": 2,
-    "name": "main",
-    "type": "int32(int32,int8**)",
-	"parameterlist": [
-		{
-			"object": "var",
-			"type": int32,
-			"name": "a"
-		},
-		...
-	],
-	"labellist": [
-		{
-			"object": "label",
-			"pos": 2
-		},
-		...
-	]
-	"body": [
-		{
-			"object": "label",
-			"pos": 2
-		},
-		...
-	]
-}
-```
+
+#label
+def labelExc(labeldata,name,pos)
+	labeldata['object'] = label
+	labeldata['name'] = name
+	labeldata['pos'] = pos
 
 
-#blockExc is used in main module, function or  process
-def blockExc(blockdata,blockNode):
-    blockdata['object']="block"
-    blockdata['filename']=blockNode.position().path
-    blockdata['line_number']=blockNode.position().lineno
-	constlist=[]
-    constlistExplain(constlist,blockNode.const_list)
-    blockdata['consts']=constlist
-	varlist=[]
-	varlistExplain(varlist,blockNode.var_list)
-	blockdata['variable']=varlist
-'''	typelist=[]
-	typelistExt(typelist,blockNode.type_list)
-    blockdata['typelist']=typelist
-	labellist=[]
-	labellistExt(labellist,blockNode.label_list)
-    blockdata['labellist']=labellist
-	funclist=[]
-	funclistExt(funclist,blockNode.func)
-    blockdata['functionlist']=funclist
-'''
-	stmtlist=[]
-	stmtlistExplain(stmtlist,blockNode.stmt)
-    blockdata['statements']=stmtlist
-'''
-block
-{
-    "object": "block",
-    "filename": "test.pas",
-    "line_number": "2",
-    "consts": [...],
-    "variable": [...],
-    "typelist": [...],
-    "labellist": [...],
-    "functionlist": [...],
-    "statements": [...]
-}'''
+#variable
+def variableExc(vardata,name,vartype,const)
+	vardata['object'] = "entity"
+	vardata['type'] = vartype
+	vardata['name'] = "variable"
+	vardata['variable'] = name
+	vardata['const'] = const
 
 
 
-
-def constdefExc(constdata,constNode):
-    constdata['object']="const_definition"
-    constdata['filename']=constNode.position().path
-    constdata['line_number']=constNode.position().lineno
-    constdata['name']=constNode.identifier
-    constdata['type']=constNode.type()
-'''
-const
-{
-    "object": "const_definition",
-    "filename": "test.cl",
-    "line_number": 3,
-    "name": "a",
-    "type": "int8",
-    
-}
-'''
-
-def vardefExc(vardata,varNode):
-    vardata['object']="variable_definition"
-    vardata['filename']=varNode.position().path
-    vardata['line_number']=varNode.position().lineno
-    vardata['name']=varNode.identifier
-    vardata['type']=varNode.type()
-'''
-variable
-{
-    "object": "variable_definition",
-    "filename": "test.cl",
-    "line_number": 3,
-    "name": "a",
-    "type": "int8",
-}
-'''
+#value
+def valueExc(valdata,valtype,value)
+	valdata['entity'] = "entity"
+	valdata['type'] = valtype
+	valdata['name'] = "value"
+	valdata['value'] = value
 
 
-# assign lhs rhs
-def asignExc(stmtdata,lhsNode,rhsNode)
-	stmtdata['object']="instruction"
-    stmtdata['filename']=lhsNode.position().path
-    stmtdata['line_number']=lhsNode.position().lineno	
-	stmtdata['name']="assign"
-	v_acdata={}
-	exprExc(v_acdata,stmtNode.var_access)
-	stmtdata['lhs']=v_acdata
-	exprdata={}
-	exprExc(exprdata,stmtNode.expr)
-	stmtdata['rhs']=exprdata
-	
-	
-#bin op left right
-def binExc(stmtdata,op,left,right)
-	stmtdata['object']="instruction"
-    stmtdata['filename']=left.position().path
-    stmtdata['line_number']=left.position().lineno	
-	stmtdata['name']="bin"
-	stmtdata['op']=op
-	leftdata={}
-	exprExc(leftdata,left)
-	stmtdata['left']=leftdata
-	rightdata={}
-	exprExc(rightdata,right)
-	stmtdata['right']=rightdata
+#variable_definition const=false
+def vardefExc(vardefdata,filename,line_number,variablename,var_type)
+	vardefdata['object'] = "instruction"
+	vardefdata['name'] = "variable_definition"
+	vardefdata['filename'] = filename
+    vardefdata['line_number'] = line_number
+	vardefdata['variablename'] = variablename
+	vardefdata['var_type'] = var_type
+	vardefdata['const'] = false
 
 
-def cjumpExc(cjumpdata,exprNode,thenlabelname,elselabelname)
-	cjumpdata['object']="instruction"
-    cjumpdata['filename']=exprNode.position().path
-    cjumpdata['line_number']=exprNode.position().lineno	
-	cjumpdata['name']="cjump"
-	exprdata={}
-	exprExc(exprdata,exprNode)
-	cjumpdata['cond']=exprdata
-	cjumpdata['thenLabel']=thenlabelname
-	cjumpdata['elseLabel']=elselabelname
+##variable_definition const=true
+def constdefExc(vardefdata,filename,Line_number,variablename,var_type,entitydata)
+	vardefdata['object'] = "instruction"
+	vardefdata['name'] = "variable_definition"
+	vardefdata['filename'] = filename
+    vardefdata['line_number'] = Line_number
+	vardefdata['variablename'] = variablename
+	vardefdata['var_type'] = var_type
+	vardefdata['const'] = true
+	vardefdata['value'] = entitydata
 
-def labelstmtExc(labelstmtdata,stmtNode,labelname)
-	labelstmtdata['object']="instruction"
-    labelstmtdata['filename']=stmtNode.position().path
-    labelstmtdata['line_number']=stmtNode.position().lineno	
-	labelstmtdata['name']="labelstmt"
-	labelstmtdata['label']=labelname
 
-def jumpExc(jumpstmt,stmtNode,labelname)
-	jumpstmt['object']="instruction"
-    jumpstmt['filename']=stmtNode.position().path
-    jumpstmt['line_number']=stmtNode.position().lineno
-	jumpstmt['name']="jump"
-	jumpstmt['label']=labelname
+
+#label_definition
+def labeldefExc(labeldefdata,filename,line_number,labelname)
+	labeldefdata['object'] = "instruction"
+	labeldefdata['name'] = "label_definition"
+	labeldefdata['filename'] = filename
+    labeldefdata['line_number'] = line_number
+	labeldefdata['labelname'] = labelname
+
+
+#load
+def loadExc(loaddata,filename,line_number,address,value)
+	loaddata['object'] = "instruction"
+	loaddata['name'] = "load"
+	loaddata['filename'] = filename
+    loaddata['line_number'] = line_number
+	loaddata['address'] = address
+	loaddata['value'] = value
+
+
+#store
+def storeExc(storedata,filename,line_number,address,value)
+	storedata['object'] = "instruction"
+	storedata['name'] = "store"
+	storedata['filename'] = filename
+	storedata['line_number'] = line_number
+	storedata['address'] = address
+	storedata['value'] = value
+
+
+#cjump
+def cjumpExc(cjumpdata,filename,line_number,cond,thenlabel,elselabel)
+	cjumpdata['object'] = "instruction"
+	cjumpdata['filename'] = filename
+	cjumpdata['line_number'] = line_number
+	cjumpdata['cond'] = cond
+	cjumpdata['thenlabel'] = thenlabel
+	cjumpdata['elselabel'] = elselabel
+
+
+#jump
+def jumpExc(jumpdata,filename,line_number,label)
+	jumpdata['object'] = "instruction"
+	jumpdata['filename'] = filename
+	jumpdata['line_number'] = line_number
+	jumpdata['label'] = label
+
+
+
+#call
+def callExc(calldata,filename,line_number,functionname,paramaterlist,value)
+	calldata['object'] = "instruction"
+	calldata['fliename'] = filename
+	calldata['line_number'] = line_number
+	calldata['functionname'] = functionname
+	calldata['paramaterlist'] = paramaterlist
+	calldata['value'] = value
+
+
+#return
+def retExc(retdata,filename,line_number,ret)
+	retdata['object'] = "instruction"
+	retdata['filename'] = filename
+	retdata['line_number'] = line_number
+	retdata['ret'] = ret
+
+
+
+#bin
+def binExc(bindata,filename,line_number,op,left,right,value)
+	bindata['object'] = "instruction"
+	bindata['filename'] = filename
+	bindata['line_number'] = line_number
+	bindata['op'] = op
+	bindata['left'] = left
+	bindata['right'] = right
+	bindata['value'] = value
+
+
+#uni
+def uniExc(unidata,filename,line_number,op,variable,value)
+	unidata['object'] = "instruction"
+	unidata['filename'] = filename
+	unidata['line_number'] = line_number
+	unidata['op'] = op
+	unidata['variable'] = variable
+	unidata['value'] = value
 
 
 
