387e65e39098a13cc7e4b2690f8a32eb0ec8aa7f
hellolzc <lzc80234@qq.com>
2017-07-03 12:12:08 +0800

    update arguments usage

--- Optimization/do_optimization.py
+++ Optimization/do_optimization.py
@@ -1,10 +1,11 @@
-#!/bin/env python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Author : lzc80234@qq.com (liuzhaoci) , axiqia
 # Created : 2017/6/19
 
 '''
-read .json and get instruction lists
+The main entrance of all optimization codes
+read a json IR and write a optimized json IR
 '''
 from __future__ import absolute_import
 from __future__ import division
@@ -74,17 +75,13 @@ def main(input_file_name, output_file_name, control_flow=False, reach_defination
     '''
     print('\nWelcome!')
     print('INPUT: %s \tOUTPUT %s'%(input_file_name, output_file_name))
-    print('Selction: \n\tcontrol_flow: %d'%control_flow)
-    print('\treach_defination: %d'%reach_defination)
-    print('\tloop: %d\n'%loop)
+    print('Selction: \n\toptimize control flow: %s'%control_flow)
+    print('\toptimize reach defination: %s'%reach_defination)
+    print('\toptimize loop: %s\n'%loop)
     with open(input_file_name, 'r') as input_file:
         ir_str = input_file.read()
         ir_json = json.loads(ir_str)
-    #print(ir_str)
-    #print(ir_json)
     #print(json.dumps(ir_json, sort_keys=True, indent=4))
-    #print(json.dumps(ir_json['body'], sort_keys=True, indent=4))
-    #print(json.dumps(ir_json['functionlist'][0]['body'], sort_keys=True, indent=4))
     for function_no, function_json in enumerate(ir_json['functionlist']):
         print('\n---------------------------------------------------------')
         print('\nparsing the body of function "%s" ...\n'%function_json["name"])
@@ -112,37 +109,50 @@ def main(input_file_name, output_file_name, control_flow=False, reach_defination
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '--input_file_name',
+        'input_file_name',
         type=str,
-        default='IR_IO/dsq.newir.json',
+        # default='IR_IO/dsq.newir.json',
         help='Input IR File name'
     )
     parser.add_argument(
-        '--output_file_name',
+        'output_file_name',
         type=str,
-        default='IR_IO/dsq.irout.json',
+        # default='IR_IO/dsq.irout.json',
         help='Output IR File name'
     )
-    parser.add_argument(
-        '--control_flow',
-        type=int,
-        default=1,
-        help='1: optimize control flow'
-    )
-    parser.add_argument(
-        '--reach_defination',
-        type=int,
-        default=1,
-        help='1: optimize reach defination'
-    )
-    parser.add_argument(
-        '--loop',
-        type=int,
-        default=1,
-        help='1: optimize loop'
-    )
-    FLAGS, unparsed = parser.parse_known_args()
-    main(FLAGS.input_file_name, FLAGS.output_file_name, bool(FLAGS.control_flow),\
-         bool(FLAGS.reach_defination), bool(FLAGS.loop))
 
+    # add conflicting options
+    group1 = parser.add_mutually_exclusive_group()
+    group1.add_argument('--control_flow', action="store_true",
+                        help='optimize control flow (default)')
+    group1.add_argument('--no_control_flow', action="store_true",
+                        help='do not optimize control flow')
+
+    group2 = parser.add_mutually_exclusive_group()
+    group2.add_argument('--reach_defination', action="store_true",
+                        help='optimize reach defination (default)')
+    group2.add_argument('--no_reach_defination', action="store_true",
+                        help='do not optimize reach defination')
+
+    group3 = parser.add_mutually_exclusive_group()
+    group3.add_argument('--loop', action="store_true",
+                        help='optimize loop (default)')
+    group3.add_argument('--no_loop', action="store_true",
+                        help='do not optimize loop')
+
+    # parse arguments
+    args = parser.parse_args()
+    # set default value
+    control_flow_flag = True
+    if args.no_control_flow:
+        control_flow_flag = False
+    reach_defination_flag = True
+    if args.no_reach_defination:
+        reach_defination_flag = False
+    loop_optimization_flag = True
+    if args.no_loop:
+        loop_optimization_flag = False
+
+    main(args.input_file_name, args.output_file_name, control_flow_flag,
+         reach_defination_flag, loop_optimization_flag)
 
